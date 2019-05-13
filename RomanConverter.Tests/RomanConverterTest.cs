e69d77dbd1d5dee0bbd72453f4e7f64dfd5240e1
Jaco Zwarts <jaco@zwarts.co.za>
2019-05-13 15:32:36 +0200

    Initial Commit

---
+++ RomanConverter.Tests/RomanConverterTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Xunit;
+
+namespace RomanConverter.Tests
+{
+    public class RomanConverterTest
+    {
+        [Fact]
+        public void ConvertI()
+        {
+            Assert.Equal(1, new RomanConverter().ConvertConvertRomanToNumericNumber("I"));
+        }
+
+        [Fact]
+        public void ConvertII()
+        {
+            Assert.Equal(2, new RomanConverter().ConvertConvertRomanToNumericNumber("II"));
+        }
+
+        [Fact]
+        public void ConvertIII()
+        {
+            Assert.Equal(3, new RomanConverter().ConvertConvertRomanToNumericNumber("III"));
+        }
+
+        [Fact]
+        public void ConvertIV()
+        {
+            Assert.Equal(4, new RomanConverter().ConvertConvertRomanToNumericNumber("IV"));
+        }
+
+        [Fact]
+        public void ConvertV()
+        {
+            Assert.Equal(5, new RomanConverter().ConvertConvertRomanToNumericNumber("V"));
+        }
+
+        [Fact]
+        public void ConvertVI()
+        {
+            Assert.Equal(6, new RomanConverter().ConvertConvertRomanToNumericNumber("VI"));
+        }
+
+        [Fact]
+        public void ConvertVII()
+        {
+            Assert.Equal(7, new RomanConverter().ConvertConvertRomanToNumericNumber("VII"));
+        }
+        [Fact]
+        public void ConvertVIII()
+        {
+            Assert.Equal(8, new RomanConverter().ConvertConvertRomanToNumericNumber("VIII"));
+        }
+
+        [Fact]
+        public void ConvertIX()
+        {
+            Assert.Equal(9, new RomanConverter().ConvertConvertRomanToNumericNumber("IX"));
+        }
+        [Fact]
+        public void ConvertX()
+        {
+            Assert.Equal(10, new RomanConverter().ConvertConvertRomanToNumericNumber("X"));
+        }
+
+        [Fact]
+        public void ConvertL()
+        {
+            Assert.Equal(50, new RomanConverter().ConvertConvertRomanToNumericNumber("L"));
+        }
+
+        [Fact]
+        public void ConvertC()
+        {
+            Assert.Equal(100, new RomanConverter().ConvertConvertRomanToNumericNumber("C"));
+        }
+
+        [Fact]
+        public void ConvertD()
+        {
+            Assert.Equal(500, new RomanConverter().ConvertConvertRomanToNumericNumber("D"));
+        }
+
+        [Fact]
+        public void ConvertM()
+        {
+            Assert.Equal(1000, new RomanConverter().ConvertConvertRomanToNumericNumber("M"));
+        }
+
+        [Fact]
+        public void ConvertXXXVI()
+        {
+            Assert.Equal(36, new RomanConverter().ConvertConvertRomanToNumericNumber("XXXVI"));
+        }
+
+        [Fact]
+        public void ConvertMMXII()
+        {
+            Assert.Equal(2012, new RomanConverter().ConvertConvertRomanToNumericNumber("MMXII"));
+        }
+
+        [Fact]
+        public void ConvertInvalidRomanValue()
+        {
+            Exception ex = Assert.Throws<Exception>(() => new RomanConverter().ConvertConvertRomanToNumericNumber("A"));
+            Assert.Contains("Invalid roman character", ex.Message);
+        }
+
+
+    }
+}
---
+++ RomanConverter/RomanConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanConverter
+{
+    public class RomanConverter
+    {
+        public RomanConverter()
+        {
+            romanSymbols.Add('I', 1);
+            romanSymbols.Add('V', 5);
+            romanSymbols.Add('X', 10);
+            romanSymbols.Add('L', 50);
+            romanSymbols.Add('C', 100);
+            romanSymbols.Add('D', 500);
+            romanSymbols.Add('M', 1000);
+        }
+
+        private IDictionary<char, int> romanSymbols = new Dictionary<char, int>();
+
+        public int ConvertConvertRomanToNumericNumber(string roman)
+        {
+            roman = roman.ToUpper();
+            int sum = 0;
+            int current = 0;
+            int previous = 0;
+
+            for (int i = 0; i < roman.ToCharArray().Length; i++)
+            {
+                if (romanSymbols.ContainsKey(roman[i]))
+                {
+                    current = romanSymbols.First(c => c.Key == roman[i]).Value;
+                    previous = i == 0 ? 0 : romanSymbols.First(c => c.Key == roman[i - 1]).Value;
+                    if (previous >= current)
+                        sum += current;
+                    else
+                    {
+                        sum -= previous;
+                        sum += (current - previous);
+                    }
+                }
+
+                else
+                    throw new Exception("Invalid roman character");
+            }
+
+            return sum;
+        }
+    }
+}
